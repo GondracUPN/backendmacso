3989b8409ad570543a6c770b1e7e09f91d88abe6
GondracUPN <n00224254@upn.pe>
2025-12-31 19:21:10 -0500

    update de gastos

--- src/gastos/dto/update-gasto.dto.ts
+++ src/gastos/dto/update-gasto.dto.ts
@@ -1,7 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGastoDto } from './create-gasto.dto';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateGastoDto extends PartialType(CreateGastoDto) {
-  // Aceptar legacy detalleGusto para compatibilidad hacia notas
+  // Aceptar legacy detalleGusto para compatibilidad hacia notas (se vuelca a notas en el service)
+  @IsOptional()
+  @IsString()
   detalleGusto?: string | null;
 }
